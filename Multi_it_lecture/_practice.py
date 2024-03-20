85df1b3408a86d57654d6027ecda1dece3286d61
unknown <https://github.com/TheBeginnerJiyeon/auto_upload.git>
2024-03-20 18:07:29 +0900

    commit

--- Multi_it_lecture/_practice.py
+++
@@ -1,8 +0,0 @@
-sentence = "Python is an amazing language"
-
-# Finding the index of substring "amazing"
-index = sentence.find("amazing")
-print("Index of 'amazing':", index)  # Output: 13
-
-print(sentence[index:index+7])
-
